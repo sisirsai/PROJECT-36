a7597681cb9a64f2fd0eae6d96ba8c0d14f37da8
SISIR SAI <73339853+sisirsai@users.noreply.github.com>
2021-01-23 20:15:57 +0530

    project 36

--- sketch.js
+++ sketch.js
@@ -3,8 +3,8 @@ var lastFed,fedTime;
 
 
 function preload(){
-  sadDog=loadImage("Images/Dog.png");
-  happyDog=loadImage("Images/happy dog.png");
+  sadDog=loadImage("images/Dog.png");
+  happyDog=loadImage("images/happy dog.png");
 }
 
 function setup() {
